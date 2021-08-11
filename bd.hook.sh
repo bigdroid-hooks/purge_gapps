854dae35e91892ce18ea5ccf6a0d438b3ecf7d39
AXON <axonasif@gmail.com>
2021-08-11 18:24:14 +0600

    refactor: Stuff according to new bigdroid design

--- bd.hook.sh
+++ bd.hook.sh
@@ -1,4 +1,51 @@
-#!/usr/bin/env bash
+#           Variables - BigDroid Internals
+#           - Only for advanced scripting -
+#           ###############################
+
+#   SRC_DIR             % The 'src' dir is the mountpoint of the project operating-system IMAGE.
+#
+#   HOOK_DIR            % This variable points to the root-dir
+#                       % of a bigdroid hook which is being run.
+#
+#   MOUNT_DIR           % The parent dir which holds other child mountpoint dirs.
+#                       % Followed by: system, secondary_ramdisk, initial_ramdisk and install_ramdisk.
+#                       % Use `SYSTEM_MOUNT_DIR`, `SECONDARY_RAMDISK_MOUNT_DIR`
+#                       % `INITIAL_RAMDISK_MOUNT_DIR`, `INSTALL_RAMDISK_MOUNT_DIR` variables for path.
+#
+#   TMP_DIR             % You can use this dir for storing temporary files.
+#                       % It's equivalent '/tmp' dir but for bigdroid hooks.
+#
+#   SECONDARY_RAMDISK   % This is either true or false aka a bolean.
+#                       % Depends on whether the project operating-system has a ramdisk.img
+#
+#   SYSTEM_IMAGE        % This points to the project system image (system.sfs or system.img) file.
+#
+#   @@ Tip: Also all the varaibles defined in the project `Bigdroid.meta` can be used.
+
+
+
+#           General Functions - BigDroid Utils
+#              - For easy hooks scripting -
+#           ##################################
+#
+#   gclone              % Copy(rsync) files preserving all their attrs with progress indicator.
+#                       % Example: `gclone "$HOOK_DIR/myfile.so" "$SYSTEM_DIR/lib64"`
+#
+#   wipedir             % Easily wipe/empty a dir(childs) without removing it's parent.
+#                       % Example: `wipedir "$INSTALL_RAMDISK_MOUNT_DIR/grub"`
+#
+#   @@ Protip: Take a look at 'src/utils.sh'
+#
+#
+#
+#
+#             libgearlock - GearLock utils
+#     - Some native gearlock vars and functions -
+#           ###############################
+#
+#   %% Simply take a look at 'https://github.com/bigdroid/bigdroid/blob/main/src/libgearlock.sh' to know
+#   %% which gearlock variables and functions are available for use.
+
 
 # Define bloat applist
 # PHOENIX_BLOAT=(
@@ -34,76 +81,76 @@
 # )
 
 GOOGLE_BLOAT=(
-"CalendarGooglePrebuilt"
-"GoogleTTS"
-"ConfigUpdater"
+	"CalendarGooglePrebuilt"
+	"GoogleTTS"
+	"ConfigUpdater"
 
-"GoogleBackupTransport"
-"GoogleContactsSyncAdapter"
-"GoogleCalendarSyncAdapter"
-"GoogleFeedback"
-"GoogleLoginService"
-"GoogleOneTimeInitializer"
-"GooglePartnerSetup"
-"GoogleServicesFramework"
-"Phonesky"
-"PrebuiltGmsCore"
-"GmsCore"
-"PicoTts"
-"BooksStub"
-"BookmarkProvider"
-"CalendarGoogle"
-"CloudPrint"
-"DeskClockGoogle"
-"EditorsDocsStub"
-"EditorsSheetsStub"
-"EditorsSlidesStub"
-"Gmail"
-"Gmail2"
-"GoogleCalendar"
-"GoogleCloudPrint"
-"GoogleHangouts"
-"GoogleKeep"
-"GoogleLatinIme"
-"Keep"
-"NewsstandStub"
-"PartnerBookmarksProvider"
-"PrebuiltBugleStub"
-"PrebuiltKeepStub"
-"QuickSearchBox"
-"Vending"
-"GmsCore"
-"GoogleNow"
-"GoogleSearch"
-"GoogleHangouts"
-"OneTimeInitializer"
-"QuickSearchBox"
-"Vending"
-"Velvet_update"
-"GmsCore_update"
-"CanvasPackageInstaller"
-"ConfigUpdater"
-"GoogleBackupTransport"
-"SetupWizard"
-"Velvet"
-"Books"
-"Chrome"
-"CloudPrint"
-"Drive"
-"Keep"
-"Movies"
-"Music"
-"NewsStand"
-"PlayGames"
-"TalkBack"
-"ClockGoogle"
-"Maps"
-"Messenger"
-"Photos"
-"Youtube"
-"CalculatorGoogle"
-"TagGoogle"
-"CarrierServices"
+	"GoogleBackupTransport"
+	"GoogleContactsSyncAdapter"
+	"GoogleCalendarSyncAdapter"
+	"GoogleFeedback"
+	"GoogleLoginService"
+	"GoogleOneTimeInitializer"
+	"GooglePartnerSetup"
+	"GoogleServicesFramework"
+	"Phonesky"
+	"PrebuiltGmsCore"
+	"GmsCore"
+	"PicoTts"
+	"BooksStub"
+	"BookmarkProvider"
+	"CalendarGoogle"
+	"CloudPrint"
+	"DeskClockGoogle"
+	"EditorsDocsStub"
+	"EditorsSheetsStub"
+	"EditorsSlidesStub"
+	"Gmail"
+	"Gmail2"
+	"GoogleCalendar"
+	"GoogleCloudPrint"
+	"GoogleHangouts"
+	"GoogleKeep"
+	"GoogleLatinIme"
+	"Keep"
+	"NewsstandStub"
+	"PartnerBookmarksProvider"
+	"PrebuiltBugleStub"
+	"PrebuiltKeepStub"
+	"QuickSearchBox"
+	"Vending"
+	"GmsCore"
+	"GoogleNow"
+	"GoogleSearch"
+	"GoogleHangouts"
+	"OneTimeInitializer"
+	"QuickSearchBox"
+	"Vending"
+	"Velvet_update"
+	"GmsCore_update"
+	"CanvasPackageInstaller"
+	"ConfigUpdater"
+	"GoogleBackupTransport"
+	"SetupWizard"
+	"Velvet"
+	"Books"
+	"Chrome"
+	"CloudPrint"
+	"Drive"
+	"Keep"
+	"Movies"
+	"Music"
+	"NewsStand"
+	"PlayGames"
+	"TalkBack"
+	"ClockGoogle"
+	"Maps"
+	"Messenger"
+	"Photos"
+	"Youtube"
+	"CalculatorGoogle"
+	"TagGoogle"
+	"CarrierServices"
 )
 # 
 # MICROG_BLOAT=(
@@ -126,7 +173,7 @@ GOOGLE_BLOAT=(
 # read -n1 -p "$(geco "\n++++ Sure to debloat ?  [${BGREEN}Y${RC} / n]  ")" c
 
 rm_bloat(){
-	geco "\n>>>> Removing ${PINK}$bloat${RC}\c" && rm -r $1/"$bloat" 2>/dev/null || geco " :: ${PINK}Failed${RC}"
+	geco "\n>>>> Removing ${PINK}$bloat${RC}\c" && rm -r "$1/$bloat" 2>/dev/null || geco " :: ${PINK}Failed${RC}"
 }
 
 # case $c in
